a7a3f11e764f77277ddfd7dc814df405a38f62ae
姚旭明 <yaoxuming@csg.cn>
2024-11-27 15:51:08 +0800

    fix:兼容导入method有空的情况

--- server.js
+++ server.js
@@ -36,9 +36,12 @@ app.get("/Main", (req, res) => {
 let config;
 
 try {
-  const configFile = fs.readFileSync(path.join(DICTIONNARY_PATH, "接口关系.json"), "utf-8");
-  // 解析 JSON 配置文件 
-  config = isValidJson(configFile) ? JSON.parse(configFile) : {data:[]} ;
+  const configFile = fs.readFileSync(
+    path.join(DICTIONNARY_PATH, "接口关系.json"),
+    "utf-8"
+  );
+  // 解析 JSON 配置文件
+  config = isValidJson(configFile) ? JSON.parse(configFile) : { data: [] };
 } catch (error) {
   console.error("读取配置文件失败:", error);
   process.exit(1); // 读取配置失败，退出程序
@@ -69,9 +72,18 @@ const readFileAndRespond = (filePath, res) => {
 config.data.forEach((apiconfig) => {
   const { method, path, apiName } = apiconfig;
   const lowerMethod = method.toLowerCase();
-  app[lowerMethod](path, (req, res) => {
-    readFileAndRespond(`${RESPONSE_PATH}/${apiName}`, res);
-  });
+  try {
+    if (!lowerMethod) {
+      console.log('\x1b[31m%s\x1b[0m', `method 有问题: ${lowerMethod}`);
+      return;
+    }
+    app[lowerMethod](path, (req, res) => {
+      readFileAndRespond(`${RESPONSE_PATH}/${apiName}`, res);
+    });
+  } catch (error) {
+    console.log('\x1b[31m%s\x1b[0m', `根据配置生成接口失败:${error}`);
+  }
+
   // console.log(`注册接口: ${method} ${path} => ${apiName}`);
   // if (lowerMethod === "get") {
   //   app.get(path, (req, res) => {
@@ -86,22 +98,31 @@ config.data.forEach((apiconfig) => {
 
 app.get("*", (req, res) => {
   console.log(`请求路径: ${req.path},没有找到，可能需要重启server.js`);
-  res
-    .status(404)
-    .send(
-      `请求路径: ${req.path} 没有找到; \n 
+  res.status(404).send(
+    `请求路径: ${req.path} 没有找到; \n 
       1.可能没有执行对应的har文件; \n
       2.可能需要重启server.js 或执行 npm run server`
-    );
+  );
 });
 
 // 启动服务器
 app.listen(PORT, () => {
   console.log(
-    "\x1b[31m%s\x1b[0m",
+    "\x1b[35m%s\x1b[0m",
     `服务器已启动，前端页面访问 http://localhost:${PORT}`
   );
   // 使用 child_process 打开浏览器
   // const start = process.platform === "win32" ? "start" : "open";
   // exec(`${start} http://localhost:${PORT}`); // 打开浏览器
 });
+
+
+/**
+console.log('\x1b[31m%s\x1b[0m', '这是红色文字'); // 红色文字
+console.log('\x1b[32m%s\x1b[0m', '这是绿色文字'); // 绿色文字
+console.log('\x1b[33m%s\x1b[0m', '这是黄色文字'); // 黄色文字
+console.log('\x1b[34m%s\x1b[0m', '这是蓝色文字'); // 蓝色文字
+console.log('\x1b[35m%s\x1b[0m', '这是紫色文字'); // 紫色文字
+console.log('\x1b[36m%s\x1b[0m', '这是青色文字'); // 青色文字
+console.log('\x1b[37m%s\x1b[0m', '这是白色文字'); // 白色文字
+*/ 
